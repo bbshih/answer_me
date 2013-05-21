607ce2babe425951aaf573f7e25b4c55fb9e3653
Billy Shih <me@billyshih.com>
2013-05-21 09:55:42 -0700

    Init and creates questions scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnswerMe::Application.config.secret_token = '897288d8c2f6f07e7800877cd65b3f44b98307cead28a4d6aa79af359318809279fc76859f6a5a845df50978a3ecc01f304258e8f6af90dc3839d102e45b597a'
